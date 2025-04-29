d095af5e1ddbb0fa207b6df70b1d4ba4565126a4
Guilherme Felipe <guilhermefelipe007@gmail.com>
2025-04-28 23:00:13 -0300

    BY: GUILHERME FELIPE & MATEO DUARTE

--- meuecommerce/src/main/java/com/meuecommerce/controller/ProdutosController.java
+++ meuecommerce/src/main/java/com/meuecommerce/controller/ProdutosController.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
